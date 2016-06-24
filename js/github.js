acef2e9c67e0cc622010315f63ad1ebf6ca5da93
Ruben <rubendal@users.noreply.github.com>
2016-06-24 15:00:48 -0500

    Open links in new tabs

--- js/github.js
+++ js/github.js
@@ -2,6 +2,6 @@
     var jsonUser = loadJSONPath("https://api.github.com/users/rubendal");
     var img_url = jsonUser.avatar_url;
     var html_url = jsonUser.html_url;
-    $('#github-user').append('<a href="' + html_url + '"><img id="github-profile" class="header-image" src="' + img_url + '"/></a>');
+    $('#github-user').append('<a href="' + html_url + '"><img id="github-profile" class="header-image" src="' + img_url + '" target="_blank"/></a>');
     $('#favicon').attr("href", img_url);
 }
